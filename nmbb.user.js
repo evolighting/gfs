90792c0083b0457cae5785db38a55171855a4d7c
evolighting <405327973@qq.com>
2016-10-16 13:31:43 +0800

    Update nmbb.user.js
    样式优化
    撤销屏蔽
    列表截断优化

--- nmbb.user.js
+++ nmbb.user.js
@@ -1,35 +1,63 @@
 // ==UserScript==
-// @name        试作
+// @name        nmbb
 // @namespace   fishcan
-// @description 惹不起，我还躲不起么
+// @description nimingban threads block
 // @include     https://h.nimingban.com/*
-// @version     1
+// @version     1.1
 // @grant       GM_getValue
 // @grant       GM_setValue
 // ==/UserScript==
+
 var blist = []
-blist = GM_getValue('blist',['0'])
-// 如果屏蔽列表太多，去掉一些老的
+blist = GM_getValue('blist',[])
+
+/************************
+*截断超过长度的屏蔽列表
+*************************/ 
 if(blist.length>100){
-  blist=blist.slice(-100)
+  blist=blist.slice(0,100)
 }
+
 var allThreads = document.getElementsByClassName("h-threads-item");
 for(var i = 0; i < allThreads.length; i++){
-  var node = document.createElement("button");
-  node.setAttribute("id",allThreads[i].getAttribute("data-threads-id"));
+  var node = document.createElement("span");
+  node.setAttribute("class","h-threads-info-report-btn");
+  var nodeA = document.createElement("a");
+  nodeA.setAttribute("id",allThreads[i].getAttribute("data-threads-id"));
   var textnode = document.createTextNode("屏蔽");
-  node.appendChild(textnode);
-  allThreads[i].firstElementChild.appendChild(node);
+  var tp = document.createTextNode("[");
+  var ta = document.createTextNode("]");
+  nodeA.appendChild(textnode);
+  node.appendChild(tp);
+  node.appendChild(nodeA);
+  node.appendChild(ta);
+  allThreads[i].firstElementChild.firstElementChild.appendChild(node);
   document.getElementById(allThreads[i].getAttribute("data-threads-id")).addEventListener('click', addblock, true);
 }
 
 function removeThreads(){
   for(var i = 0; i < allThreads.length; i++){
     var thisId = allThreads[i].getAttribute("data-threads-id");
-    if(blist.indexOf(thisId)>-1){
-      allThreads[i].remove()
+    var thisNode = allThreads[i];
+    var thisIndex = blist.indexOf(thisId)
+    if(thisIndex>-1){
+      // 移除串内容改为替换为撤销按钮
+      while(thisNode.hasChildNodes()){
+        thisNode.removeChild(thisNode.lastChild)
+      }
+      var node = document.createElement("span");
+      node.setAttribute("class","h-threads-info-reply-btn");
+      node.innerHTML = "[<a>撤销屏蔽</a>]";
+      node.firstElementChild.setAttribute("id",thisId);
+      
+      thisNode.appendChild(node)
+      document.getElementById(thisId).addEventListener('click', cancelBlock, true)
+      // 将被使用到的规则移到前面
+      blist.splice(thisIndex,1);
+      blist.unshift(thisId);
     }
   }
+  GM_setValue('blist',blist)
 }
 
 function  addblock(e){
@@ -38,6 +66,17 @@ function  addblock(e){
   blist.push(thisId);
   GM_setValue('blist',blist)
   removeThreads();
+}
+
+function cancelBlock(e){
+  e.stopPropagation();
+  var thisId = this.getAttribute("id");
+  var thisIndex = blist.indexOf(thisId);
+  if(thisIndex>-1){
+    blist.splice(thisIndex,1)
+    GM_setValue('blist',blist)
+  }
+  location.reload(); 
 }
 
 removeThreads()
