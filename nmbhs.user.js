ae0104a4d3f686f8f493d27c5fd13d68ad319771
evolighting <405327973@qq.com>
2016-10-18 12:18:42 +0800

    Add files via upload
    隐藏被sage的串的内容

---
+++ nmbhs.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name        nmbhs
+// @namespace   fishcan
+// @description hide sage threads
+// @include     https://h.nimingban.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+
+var sageThreads = [];
+var sageIndex = [];
+
+var sageIcon = document.getElementsByClassName("uk-icon-thumbs-down");
+// 由于js的动态性，如果dom中节点消失了，那么其对应的集合也会消失；
+//（其实这是我瞎编的，但是一下代码的效果上来说就是这样）
+while(sageIcon.length > 0){
+  var thisNode = sageIcon[0].parentNode.parentNode;
+  var thisId = thisNode.getAttribute("data-threads-id");
+  var cNode = document.createElement("div");
+  cNode.setAttribute("class","h-threads-item uk-clearfix");
+  var node = document.createElement("span");
+  var blank = document.createElement("span");
+  node.setAttribute("class","h-threads-info-reply-btn uk-text-danger uk-text-bold");
+  node.innerHTML = "[<a style='color:#d85030'>==sage==</a>]";
+  node.firstElementChild.setAttribute("id",thisId);
+  cNode.appendChild(node);
+  sageThreads.push(thisNode.cloneNode(true));
+  sageIndex.push(thisId);
+  thisNode.parentNode.replaceChild(cNode,thisNode);
+  document.getElementById(thisId).addEventListener('click', toggleHideSage, true);
+}
+
+function toggleHideSage(e){
+  var thisThreads = this.parentNode.parentNode
+  e.stopPropagation();
+  var index = sageIndex.indexOf(this.getAttribute("id"));
+  var node = document.createElement("span");
+  if(thisThreads.firstElementChild.nextElementSibling == null){
+    thisThreads.appendChild(sageThreads[index]);
+  }else{
+    thisThreads.removeChild(thisThreads.firstElementChild.nextElementSibling);
+  }
+}
